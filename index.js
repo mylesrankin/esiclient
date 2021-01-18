491ff7adfebd94e7efa1a1c57da7a7cf5d075c73
Myles Rankin <me@mylesrankin.uk>
2021-01-18 00:12:24 +0000

    started authed calls support

--- index.js
+++ index.js
@@ -3,14 +3,39 @@ const bunyan = require('bunyan');
 const log = bunyan.createLogger({ name: "ESIClient" });
 
 class ESIClient {
-    constructor(esiBaseUrl, credentials, proxy = false) {
+    constructor(esiBaseUrl, { credentials, proxy }) {
         this.baseUrl = esiBaseUrl
         this.limitReset = 60
         this.limitRemain = 100
         this.log = log.child({ baseUrl: this.baseUrl, limitReset: this.limitReset, limitRemain: this.limitRemain })
+        this.credentials = credentials
+        this.proxy = proxy
     }
 
-    async refreshCredentials() { }
+    async refreshCredentials() {
+        const { clientId, clientSecret, refresh_token } = this.credentials
+        const base64ClientString = Buffer.from(`${clientId}:${clientSecret}`).toString("base64")
+        const config = {
+            url: "https://login.eveonline.com/oauth/token",
+            method: "POST",
+            body: {
+                "grant_type": "refresh_token",
+                "refresh_token": refresh_token
+            },
+            headers: {
+                "Authorization": `Basic ${base64ClientString}`,
+                "Content-Type": "application/json"
+            }
+        }
+        try {
+            const result = await axios(config)
+            this.credentials = result.data
+            var d = new Date('2014-01-01 10:11:55');
+            this.credentials.expiry = new Date(new Date().getTime() + result.data.expires_in * 1000)
+        } catch (e) {
+            throw new Error(`Error when refreshing ESI credentials: ${e.message}`)
+        }
+    }
 
     async sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms))
@@ -21,7 +46,7 @@ class ESIClient {
         this.limitReset = headers['x-esi-error-limit-reset']
     }
 
-    async request(route, method, headers, authorization = false, attempt = 1) {
+    async request(route, method, headers, authRequired = false, attempt = 1) {
         const url = this.baseUrl + route
         const config = {
             url,
@@ -30,10 +55,16 @@ class ESIClient {
             ...(headers ? { headers } : {})
         }
         if (this.limitRemain > 10) {
+            if (authRequired) {
+                if (this.credentials.expiry < new Date(new Date().getTime() + 60 * 1000)) {
+                    await this.refreshCredentials()
+                }
+                config.headers.Authorization = `Bearer ${this.credentials.access_token}`
+            }
             try {
                 const result = await axios(config)
                 this.log.info(`Recieved data for ${method} ${url}`, result)
-                this.saveErrorLimits(result.headers)
+                this.saveErrorLimits(result.headers)--
                 return result
             } catch (e) {
                 this.log.error(`An error occured when querying ${method} ${url}: ${e.message}`, e)
@@ -54,7 +85,7 @@ class ESIClient {
         } else {
             this.log.warn(`Close to esi-error-limit, throttling request ${method} ${url} for ${this.limitReset} seconds`)
             await this.sleep(this.limitReset * 1000)
-            return this.request(route, method, headers, authorization, attempt + 1)
+            return this.request(route, method, headers, authRequired, attempt + 1)
         }
     }
 }
